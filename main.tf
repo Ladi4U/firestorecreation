67d8f566839257dac71a95162baf6880e1abcb96
Sami Islam <islam.samipkr@gmail.com>
2023-03-31 15:27:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "google"{}
 resource "google_firestore_database""database" {
   project="firestore-database-creation"
   name="(default)"
-  location_id="US"
+  location_id="nam5"
   type="FIRESTORE_NATIVE"
 }
